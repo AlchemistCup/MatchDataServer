12d5dfb0e4dc164d49dc011590ac60bbd3187237
simon-staal <simonstaal10@gmail.com>
2023-05-27 17:51:32 +0000

    Added test implementations for web server endpoints

--- web_server.py
+++ web_server.py
@@ -18,12 +18,59 @@ class HTTPServer:
     def _setup_routes(self):
         routes = web.RouteTableDef()
 
-        @routes.get('/start')
-        async def start_match(request: web.Request):
-            self._logger.info(f"Received match start request with player1 = {request.query.get('p1')} and player2 = {request.query.get('p2')}")
+        @routes.get('/setup')
+        async def setup_match(request: web.Request):
+            self._logger.debug(f"Received match setup request with player1 = {request.query.get('p1')} and player2 = {request.query.get('p2')}")
             matchdata.n_of_requests += 1
             self._logger.info(f"Processed {matchdata.n_of_requests} total requests")
-            response = web.json_response({'error': 'Insufficient boards'})
+            #response = web.json_response({'error': 'Insufficient boards'})
+            response = web.json_response({
+                "body": {
+                    "match_id": "TestMatchId"
+                }
+            })
+            return response
+        
+        @routes.get('/end-turn')
+        async def end_turn(request: web.Request):
+            self._logger.debug(f"Received end_turn request {request.query}")
+            response = web.json_response({
+                "body": {
+                    "score": 15,
+                    "blanks": 1
+                }
+            })
+            return response
+
+        @routes.get('/challengeable-words')
+        async def get_challengeable_words(request: web.Request):
+            self._logger.debug(f"Received get_challengeable_words request {request.query}")
+            response = web.json_response({
+                "body": {
+                    "words": ["HELLO", "GOODBYE", "ASDFQGE"]
+                }
+            })
+            return response
+        
+        @routes.get('/challenge')
+        async def challenge(request: web.Request):
+            self._logger.debug(f"Received challenge request {request.query}")
+            response = web.json_response({
+                "body": {
+                    "successful": False,
+                    "penalty": 0
+                }
+            })
+            return response
+        
+        @routes.post('/blanks')
+        async def update_blank_tiles(request: web.Request):
+            self._logger.debug(f"Received blank tile update {request.query}, has body = {request.can_read_body}")
+            body = await request.text()
+            self._logger.debug(f"Request body = {body}")
+            response = web.json_response({
+                "body": {}
+            })
             return response
         
         self._app.add_routes(routes)
